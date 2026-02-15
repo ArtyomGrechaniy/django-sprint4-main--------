1327e31075864d69ab59eef6aaa507b9d13cdda6
Artyom Grechaniy <gyroexual@gmail.com>
2026-02-15 23:28:38 +0300

    b

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
-
-from . import views
+from users import views as userviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('auth/', include('django.contrib.auth.urls')),
-    path('auth/registration/', views.RegistrationView.as_view(),
-         name='registration'),
     path('pages/', include('pages.urls')),
+    path('auth/registration/', userviews.RegistrationView.as_view(),
+         name='registration'),
 ]
 
 handler403 = 'pages.views.csrf_failure'
--- blogicum/users/views.py
+++ blogicum/users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from users.forms import UserCreationForm
 
-# Create your views here.
+
+class RegistrationView(CreateView):
+    template_name = 'registration/registration_form.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('blog:index')
