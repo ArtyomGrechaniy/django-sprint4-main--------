d63cab63a6b9a790cf6fc56d02e5724f40c466cb
Artyom Grechaniy <gyroexual@gmail.com>
2026-02-16 00:22:28 +0300

    и

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -149,7 +149,8 @@ class PostUpdateView(PostMixin, UpdateView):
 
     def get_success_url(self):
         return reverse(
-            'blog:post_detail', kwargs={'post_id': self.pk_url_kwarg}
+            'blog:post_detail',
+            kwargs={'post_id': self.kwargs['post_id']}
         )
 
 
